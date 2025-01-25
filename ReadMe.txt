1e27ce487852bef11d1c8405df342a5983cabfdb
박주현 <bagjuhyeon@bagjuhyeon-ui-MacBookPro.local>
2025-01-25 15:04:13 +0900

    Your First JS Proj

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,2 @@
-This is a test
-This is a dev branch
+도장
+1. 01/25
---
+++ app.js
@@ -0,0 +1,5 @@
+// alert in browser
+alert("Hello World");
+//근데 왜 위의 결과로는 browser에서 alert 창이 뜨지 않는거지?
+//console.log("Hello World");
+//console.log(1+1)
