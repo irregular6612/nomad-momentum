4cb7b0f9b10133021cf137c221d5d7c8bef629be
박주현 <bagjuhyeon@bagjuhyeon-ui-MacBookPro.local>
2025-01-28 21:06:19 +0900

    data type

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
-// alert in browser
-alert("Hello World");
-//근데 왜 위의 결과로는 browser에서 alert 창이 뜨지 않는거지?
-//console.log("Hello World");
-//console.log(1+1)
+console.log(2+2);
+console.log("hello " + "world");
