9042a18e61535ab8ca6c2fcc0407c459c829e269
박주현 <bagjuhyeon@bagjuhyeon-ui-MacBookPro.local>
2025-02-16 12:41:57 +0900

    arrays

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log(2+2);
-console.log("hello " + "world");
+let a = [1,2,3];
+a.push(4);
+console.log(a);
+a.pop();
+console.log(a);
+a.shift();
+console.log(a);
+a.unshift(1);
+console.log(a);
+
+
+
+
+
+
+
