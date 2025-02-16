979c489ff97ab6318fa6cf83ebfd7ba43c61b10f
박주현 <bagjuhyeon@bagjuhyeon-ui-MacBookPro.local>
2025-02-16 13:01:25 +0900

    conditional

--- app.js
+++ app.js
@@ -16,8 +16,16 @@ const calculator = {
     }
 };
 
-console.log(calculator.plus(1, 2));
-console.log(calculator.minus(1, 2));
-console.log(calculator.multiply(1, 2));
-console.log(calculator.divide(1, 2));
-console.log(calculator.power(1, 2));
+//parseInt() : 문자열을 숫자로 변환 단, 숫자 이외의 char직전까지만 변환
+console.log(parseInt('fk123')) //-> NaA
+console.log(parseInt('123'), parseInt('12ffd2')); // 123, 12
+console.log(typeof parseInt('123.456')); // 123
+
+//isNaN() : 숫자가 아닌 경우 true, 숫자인 경우 false
+//parseInt()의 arg로 들어간 'fk123'의 첫 char가 숫자가 아니므로, NaN이 반환됨
+if (isNaN(parseInt('fk123'))) {
+    console.log('숫자가 아닙니다.');
+} else {
+    console.log('숫자입니다.');
+}
+
